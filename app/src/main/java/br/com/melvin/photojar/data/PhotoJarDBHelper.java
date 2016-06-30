7fc0703896b5aa5b5b90d1ebd63d6e2c009b4412
DB Conrado <dbconrado@gmail.com>
2016-06-30 20:21:51 -0300

    SQLite Helper for local storage

---
+++ app/src/main/java/br/com/melvin/photojar/data/PhotoJarDBHelper.java
@@ -0,0 +1,50 @@
+package br.com.melvin.photojar.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by DB on 30/06/2016.
+ */
+public class PhotoJarDBHelper extends SQLiteOpenHelper {
+
+
+    private static final String DATABASE_NAME = "photojar.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String CREATE_PHOTO_TABLE =
+            "CREATE TABLE photo (" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "timestamp INTEGER NOT NULL," +
+                    "path TEXT NOT NULL)";
+
+    private static final String CREATE_TAG_TABLE =
+            "CREATE TABLE tag (" +
+                    "name TEXT PRIMARY KEY," +
+                    "color INTEGER NOT NULL)";
+
+    private static final String CREATE_PHOTOTAG_TABLE =
+            "CREATE TABLE phototag (" +
+                    "photo_id INTEGER PRIMARY KEY," +
+                    "tag_name TEXT PRIMARY KEY)";
+
+    public PhotoJarDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_PHOTO_TABLE);
+        sqLiteDatabase.execSQL(CREATE_TAG_TABLE);
+        sqLiteDatabase.execSQL(CREATE_PHOTOTAG_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS photo");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS tag");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS phototag");
+        onCreate(sqLiteDatabase);
+    }
+}
